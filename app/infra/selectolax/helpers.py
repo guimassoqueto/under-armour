a52db68225c8c807f6bd5e597f4a36cebd9be8a1
guimassoqueto <gui.massoqueto@gmail.com>
2023-09-15 08:47:32 -0300

    chore: update string replace

--- app/infra/selectolax/helpers.py
+++ app/infra/selectolax/helpers.py
@@ -2,7 +2,7 @@ import re
 
 
 def get_image_url(small_image_url: str) -> str:
-  return small_image_url.replace("520-520", "1080-auto").replace("width=520", "width=1080").replace("height=520", "height=1080")
+  return small_image_url.replace("520-520", "1080-auto").replace("width=520", "width=1080").replace("height=520", "height=auto")
 
 
 def get_price(inner_text: str) -> str:
